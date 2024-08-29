8f3d981710baad1ea6e9ff9bfa00478e51b3d61e
ZaynAnwar <zaynanwar100@gmail.com>
2024-08-29 15:41:46 +0500

    Update login.js

--- login/login.js
+++ login/login.js
@@ -1,26 +1,32 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Get social login buttons
     var googleBtn = document.querySelector(".google");
     var facebookBtn = document.querySelector(".facebook");
     var appleBtn = document.querySelector(".apple");
   
-    // Add event listeners to handle button clicks
     googleBtn.addEventListener("click", function(event) {
       event.preventDefault();
-      // Handle Google sign up (you can implement your logic here)
       console.log("Signing up with Google...");
     });
   
     facebookBtn.addEventListener("click", function(event) {
       event.preventDefault();
-      // Handle Facebook sign up (you can implement your logic here)
       console.log("Signing up with Facebook...");
     });
   
     appleBtn.addEventListener("click", function(event) {
       event.preventDefault();
-      // Handle Apple sign up (you can implement your logic here)
       console.log("Signing up with Apple...");
     });
   });
   
+
+  document.getElementById('loginForm').addEventListener('submit', function(event) {
+    const userType = document.querySelector('input[name="userType"]:checked').value;
+    console.log(`User Type: ${userType}`);
+
+    if (userType === 'serviceSeeker') {
+
+    } else if (userType === 'serviceProvider') {
+
+    }
+});
